ddf9b0bc1663dab32d23a4c425ccc112ff8a23db
pleopard93 <pleopard93@gmail.com>
2014-03-14 17:12:12 -0500

    Re-added Xcode files

---
+++ iOS/Lightwait/Lightwait/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Lightwait
+//
+//  Created by Patrick Leopard II on 3/9/14.
+//  Copyright (c) 2014 Patrick Leopard II. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
