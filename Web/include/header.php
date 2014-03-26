79f3e8a89650a55720a21b7b483b9550fc177ba9
pleopard93 <pleopard93@gmail.com>
2014-03-26 11:03:42 -0500

    Put back the nav bar changed in previous commit

--- Web/include/header.php
+++ Web/include/header.php
@@ -11,57 +11,37 @@
 
    $app = new \Slim\Slim();
 
-   $app->get('/wines', 'getWines');
-   $app->run();
-
-
-function getWines() {
-   echo "Hello";
-
-}
-
-function getConnection() {
-   $dbhost="localhost";
-   $dbuser="root";
-   $dbpass="root";
-   $dbname="Macs";
-   $dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);  
-   $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-   return $dbh;
-}
-
-
    /*$app->get('/Orders', function() use ($db, $app) {
-   		$query = "SELECT Users.fName, Users.lName, Orders.timePlaced, Orders.OrderID, Breads.name, Bases.name, Cheeses.name, Orders.hasFries 
-				FROM Orders INNER JOIN Users ON Orders.UserID = Users.UserID INNER JOIN Breads ON Orders.BreadID = Breads.BreadID
-				INNER JOIN Bases ON Orders.BaseID = Bases.BaseID INNER JOIN Cheeses ON Orders.CheeseID = Cheeses.CheeseID ORDER BY Orders.OrderID";
-   		
-   		$result = mysqli_query($db, $query);
-   		$orders = mysqli_fetch_object($result);
-   		echo json_encode($orders);
+         $query = "SELECT Users.fName, Users.lName, Orders.timePlaced, Orders.OrderID, Breads.name, Bases.name, Cheeses.name, Orders.hasFries 
+            FROM Orders INNER JOIN Users ON Orders.UserID = Users.UserID INNER JOIN Breads ON Orders.BreadID = Breads.BreadID
+            INNER JOIN Bases ON Orders.BaseID = Bases.BaseID INNER JOIN Cheeses ON Orders.CheeseID = Cheeses.CheeseID ORDER BY Orders.OrderID";
+         
+         $result = mysqli_query($db, $query);
+         $orders = mysqli_fetch_object($result);
+         echo json_encode($orders);
    });
-$app->run();*/
+   $app->run();*/
 ?>
 
 <!Doctype html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<title><?php echo $pageTitle ?></title>
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<link href='http://fonts.googleapis.com/css?family=Ubuntu:400,500' rel='stylesheet' type='text/css'>
+   <meta charset="utf-8">
+   <title><?php echo $pageTitle ?></title>
+   <link rel="stylesheet" type="text/css" href="css/style.css">
+   <link href='http://fonts.googleapis.com/css?family=Ubuntu:400,500' rel='stylesheet' type='text/css'>
 
-	<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
-	<script src="javascript/main.js"></script>
+   <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+   <script src="javascript/main.js"></script>
 </head>
 
 <body>
-	<nav>
-		<h1>lightwait</h1>
-		<ul>
-			<?php
-				foreach($navElements as $key => $value) {
-					echo '<li><a href='.$value.'>'.$key.'</a></li>';
-			} ?>
-		</ul>
-	</nav>
+   <nav>
+      <h1>lightwait</h1>
+      <ul>
+         <?php
+            foreach($navElements as $key => $value) {
+               echo '<li><a href='.$value.'>'.$key.'</a></li>';
+         } ?>
+      </ul>
+   </nav>
