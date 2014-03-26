1442267c633726ef4b19a4f0bcc3b540a77eb8b9
LukeOglesbee <loglesbee@smu.edu>
2014-03-26 00:38:06 -0500

    Fixed merge conlflict

--- Web/index.php
+++ Web/index.php
@@ -6,11 +6,6 @@
 
 
 <body>
-<<<<<<< HEAD
-	<h1>Login home page</h1>
-	<a href="queue.php">Chef</a>
-	<a href="order.php">Order</a>
-=======
 	<div class="login">
 		<h1>Log In</h1>
 		<form id="loginForm" method="POST">
@@ -19,7 +14,7 @@
 			<input class="formSubmit" type="submit" value="Log In">
 		</form>	
 	</div>
-	<a href="queue.php">Screw that, I'll hack in!</a>
->>>>>>> 6aea8371f6c6230abb1f0282a016afabe7f3d488
+	<a href="queue.php">Chef</a>
+	<a href="order.php">Order</a>
 </body>
 </html>
