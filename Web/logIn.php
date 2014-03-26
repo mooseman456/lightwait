169fab37d3ce73b8e9d0299b1da2208e58a2dbe5
soxymo <soxymo@aol.com>
2014-03-25 19:49:27 -0500

    Added basic logIn.php

---
+++ Web/logIn.php
@@ -0,0 +1,16 @@
+<?php
+	$pageTitle = "availability";
+	include 'include/header.php';
+?>
+</head>
+<body>
+	<div class="login">
+		<h1>Log In</h1>
+		<form id="loginForm" method="POST">
+			<input class="textForm" type="text" name="username" placeholder="Username" required><br/>
+			<input class="textForm" type="text" name="password" placeholder="Password" requried><br/>
+			<input class="textForm" type="submit" value="Log In">
+		</form>	
+	</div>
+</body>
+</html>
