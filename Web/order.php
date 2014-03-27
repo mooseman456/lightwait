373e852ce6b0c9ba150186e8efa2ccb9af34710b
LukeOglesbee <loglesbee@smu.edu>
2014-03-26 22:22:53 -0500

    more queue styling

--- Web/order.php
+++ Web/order.php
@@ -3,6 +3,14 @@
 	$navElements=array("account"=>"#",logout=>"index.php");
 	include('include/header.php');
 ?>
+	<div class="order-wrapper">
+		<div class="order-page">
+			<h1>Order</h1>
+		</div>
+		<div class="order-page">
+			<h1>Menu</h1>
+		</div>
+	</div>
 </body>
 
 </html>
