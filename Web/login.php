56e15ae22c2ddb989af106ae6505363861e0258b
moosemn456 <jgstangelo@gmail.com>
2014-03-27 19:16:05 -0500

    Added stuff

--- Web/login.php
+++ Web/login.php
@@ -9,7 +9,7 @@
 		<h1>Log In</h1>
 		<form id="loginForm" method="POST">
 			<input class="textForm" type="text" name="username" placeholder="Username" required><br/>
-			<input class="textForm" type="text" name="password" placeholder="Password" requried><br/>
+			<input class="password" type="password" name="password" placeholder="Password" requried><br/>
 			<input class="formSubmit" type="submit" value="Log In">
 		</form>	
 	</div>
