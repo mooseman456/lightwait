919db5e63850f084e9336d402a7a547295f84a9a
moosemn456 <jgstangelo@gmail.com>
2014-03-29 16:21:18 -0500

    Order Menu dynamically created

--- Web/javascript/main.js
+++ Web/javascript/main.js
@@ -114,8 +114,8 @@ $(document).ready(function(){
 
       maxPage = Math.floor(numOrders/10);
       changePage();
-   }
-});
+      }
+   });
 
 // UPDATE SIDEBAR
 // Takes an array of JSON orders
@@ -165,27 +165,36 @@ function loadAvailChat(vClient){
   
    var rootURL = "http://localhost/lightwait/Web/api/index.php/menu";
 
-   function getMenuDat() {
+   function getMenuData() {
       $.ajax({
          type: 'GET',
          url: rootURL,
          dataType: "json", // data type of response
          success: function(data){  
-            //console.log("Chicken!");
+            console.log("Chicken!");
             $('#menuForm').append("<ul id=\"basesMenu\">");  
             for (var i=0; i<data['Bases'].length; i++){
                $('#menuForm').append("<li> <input type=\"radio\" name=\"baseType\" id=\"" + data['Bases'][i] + "\" value=\"" + data['Bases'][i] + "\"> <label for=\"" + data['Bases'][i] + "\">" + data['Bases'][i] + "</label></li>");
             }
             $('#menuForm').append("</ul><ul id=\"breadsMenu\">");
             for (var i=0; i<data['Breads'].length; i++){
-               $('#menuForm').append("<li> <input type=\"radio\" name=\"breadsType\" id=\"" + data['Breads'][i] + "\" value=\"" + data['Breads'][i] + "\"> <label for=\"" + data['Breads'][i] + "\">" + data['Breads'][i] + "</label></li>");
+               $('#menuForm').append("<li> <input type=\"radio\" name=\"breadType\" id=\"" + data['Breads'][i] + "\" value=\"" + data['Breads'][i] + "\"> <label for=\"" + data['Breads'][i] + "\">" + data['Breads'][i] + "</label></li>");
+            }
+            $('#menuForm').append("</ul><ul id=\"cheeseMenu\">");
+            for (var i=0; i<data['Cheeses'].length; i++){
+               $('#menuForm').append("<li> <input type=\"radio\" name=\"cheeseType\" id=\"" + data['Cheeses'][i] + "\" value=\"" + data['Cheeses'][i] + "\"> <label for=\"" + data['Cheeses'][i] + "\">" + data['Cheeses'][i] + "</label></li>");
+            }
+            $('#menuForm').append("</ul><ul id=\"toppingsMenu\">");
+            for (var i=0; i<data['Toppings'].length; i++){
+               $('#menuForm').append("<li> <input type=\"checkbox\" name=\"toppingType\" id=\"" + data['Toppings'][i] + "\" value=\"" + data['Toppings'][i] + "\"> <label for=\"" + data['Toppings'][i] + "\">" + data['Toppings'][i] + "</label></li>");
             }
             $('#menuForm').append("</ul>");
+
          }
       });
    }
 
-   getMenuDat();
+   getMenuData();
    
    function postOrder() {
       console.log('addWine');
