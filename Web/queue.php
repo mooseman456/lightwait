31e3c7341c814a3076440e39c7670d82917da5be
LukeOglesbee <loglesbee@smu.edu>
2014-03-30 21:31:25 -0500

    update

--- Web/queue.php
+++ Web/queue.php
@@ -13,7 +13,7 @@
 		<button id="recall">Recall</button>
 		<div class="navigation">
 			<img src="images/prev_arrow.png" alt="Previous Arrow" />
-			<div id="page_number">1/3</div>
+			<div id="page_number">1/1</div>
 			<img src="images/next_arrow.png" alt="Next Arrow" />
 		</div>
 	</div>
