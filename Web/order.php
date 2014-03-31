61cb39807fa8f96dacc2a795d49c39469882b447
moosemn456 <jgstangelo@gmail.com>
2014-03-30 23:56:30 -0500

    FIXED LINK

--- Web/order.php
+++ Web/order.php
@@ -6,7 +6,7 @@
 	<div class="order-wrapper">
 		<p>Add items to your order by clicking on them in the menu</p>
 		<div id="menuWrapper" class="order-page">
-			<form id="menuForm" method="POST" action="http://54.186.252.120.com/Web/api/index.php">
+			<form id="menuForm" method="POST" action="http://54.186.252.120.com/Web/api/index.php/webOrder">
 			</form>
 		</div>
 		
