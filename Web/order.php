49bf5da3b4761f1ccd9910cc04615c7ac09afc5d
LukeOglesbee <loglesbee@smu.edu>
2014-03-30 21:16:42 -0500

    poop

--- Web/order.php
+++ Web/order.php
@@ -7,14 +7,6 @@
 		<p>Add items to your order by clicking on them in the menu</p>
 		<div id="menuWrapper" class="order-page">
 			<form id="menuForm" method="POST" action="http://localhost/lightwait/Web/api/index.php/webOrder">
-				<!-- <ul id="basesMenu">
-					<li>
-						<input type="radio" name="baseType" id=jsonName value=jsonName>
-						<label for=jsonName>jsonName</label>
-					</li>
-
-				</ul>
-				<ul id="toppingsMenu"> -->
 			</form>
 		</div>
 		
