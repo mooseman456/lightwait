5a646ed1f4f3a49399a0f992b41dbe136f6d3efa
LukeOglesbee <loglesbee@smu.edu>
2014-04-04 20:33:16 -0500

    Too much ruby...

--- Web/order.php
+++ Web/order.php
@@ -1,7 +1,7 @@
 <?php
 	$pageTitle = "Order";
 	$navElements=array("account"=>"account.php","logout"=>"index.php");
-	$javascript='javascript/main'
+	$javascript='javascript/main';
 	include('include/header.php');
 ?>
 	<div class="order-wrapper">
