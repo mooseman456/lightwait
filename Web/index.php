b286dbdbf46135b3089c433ff1fa237ad2cf47f1
moosemn456 <jgstangelo@gmail.com>
2014-04-09 16:17:56 -0500

    Added some more regex cause it's kinda cool.

--- Web/index.php
+++ Web/index.php
@@ -9,7 +9,7 @@
       <div class="accountForm">
    		<h1>Log In</h1>
    		<form id="loginForm" method="POST" action="#">
-   			<input class="textForm" type="email" name="email" placeholder="Email" required ><br/>
+   			<input class="textForm" type="email" name="email"  placeholder="Email" required ><br/>
    			<input class="textForm" type="password" name="password" placeholder="Password" pattern=".{8,20}" required><br/>
    			<input class="formSubmit" type="submit" value="Log In">
    		</form>
@@ -18,7 +18,7 @@
             <h1>Create an Account</h1>
             <form id="createAccountForm" method="POST" action="#">
                 <input class="textForm" type="email" name="email" placeholder="Email" required>
-                <input class="textForm" type="password" name="password" placeholder="Password" required>
+                <input class="textForm" type="password" name="password" pattern=".{8,20}" placeholder="Password" required>
                 <input type="text" name="fName" placeholder="First Name" required>
                 <input type="text" name="lName" placeholder="Last Name" required>
                 <input type="tel" name="phone" placeholder="Phone Number" required>
--- Web/javascript/account.js
+++ Web/javascript/account.js
@@ -9,35 +9,68 @@ const rootURL = "api/index.php"
 
 $(document).ready(function() {
 
+    // Declards regex patters for different portions of the account creation/login
+    var passPat = /.{8,20}/;
+
     $('#loginForm input[type="submit"]').click(function(e) {
         e.preventDefault();
         var email = $('#loginForm input[name="email"]').val();
         var password = $('#loginForm input[name="password"]').val();
 
-        var passPat = /.{8,20}/;
-        password +="";
-        if (passPat.test(password)) {
-            console.log("It works!");
+        if (passPat.test(password))
             logIn(email, password);
-        }
+
         else
-            console.log("It fails!");
+            alert("Invalid password! Must be 8 - 20 characters long!");
     });
 
     $('#createAccountForm input[type="submit"]').click(function(e) {
         e.preventDefault();
-        var fName = $('#createAccountForm input[name="fName"]').val();
-        var lName = $('#createAccountForm input[name="lName"]').val();
+        var createValid = true;
+        var errorString = "";
+        
         var email = $('#createAccountForm input[name="email"]').val();
+        if (email === "") {
+            errorString += "Email empty!\n";
+            createValid = false;
+        }
+        
         var password = $('#createAccountForm input[name="password"]').val();
+        if (!passPat.test(password)) {
+            errorString += "Invalid password! Must be 8 - 20 characters long!\n";
+            createValid = false;
+        }
+        
+        var fName = $('#createAccountForm input[name="fName"]').val();
+        if (fName === "") {
+            errorString += "First name empty!\n";
+            createValid = false;
+        }
+       
+        var lName = $('#createAccountForm input[name="lName"]').val();
+        if (lName === "") {
+            errorString += "Last name empty!\n";
+            createValid = false;
+        }
+        
         var phone = $('#createAccountForm input[name="phone"]').val();
+        if (phone === "") {
+            errorString += "Phone number empty!\n";
+            createValid = false;
+        }
         console.log(fName+" "+lName+" "+email+" "+password+" "+phone);
-        createAccount(fName, lName, email, password, phone);
+        
+        if (createValid === false)
+            alert(errorString);
+
+        else
+            createAccount(fName, lName, email, password, phone);
     });
 
     $('#editAccountForm input[type="submit"]').click(function(e) {
         e.preventDefault();
         var fName = $('#editAccountForm input[name="fName"]').val();
+
         var lName = $('#editAccountForm input[name="lName"]').val();
         var email = $('#editAccountForm input[name="email"]').val();
         var password = $('#editAccountForm input[name="password"]').val();
