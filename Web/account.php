e452252fb8997bc4ff4184507a9b18f491b49fb1
LukeOglesbee <loglesbee@smu.edu>
2014-04-08 19:20:25 -0500

    editAccount ajax

--- Web/account.php
+++ Web/account.php
@@ -1,10 +1,11 @@
 <?php
    $pageTitle="Account";
    $navElements = Array("home"=>"index.php","queue" => "queue.php");
+   $javascript = 
    include('include/header.php');
 ?>
 
-	<form id="editAccountForm" method="POST" action="#">
+	<form id="editAccountForm" method="PUT" action="#">
 		<input type="email" name="email" placeholder="Email">
 		<input type="password" name="password" placeholder="Password">
 		<input type="text" name="fName" placeholder="First Name">
