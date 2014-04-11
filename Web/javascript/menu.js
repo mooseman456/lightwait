53702f9148a782871b91f0b54c766df980b22c16
LukeOglesbee <loglesbee@smu.edu>
2014-04-11 13:42:18 -0500

    Availability deleted, all menu/availability functionality for chef and admin should now be in menu.js. TODO: inflater for admin menu.

--- Web/javascript/menu.js
+++ Web/javascript/menu.js
@@ -8,23 +8,26 @@ $(document).ready(function(){
 /************/
 /*   AJAX   */
 /************/
+
+//Get menu data from the database and call inflate function
 function getMenuData() {
     $.ajax({
         type: 'GET',
-        url: rootURL+"/menu",
+        url: rootURL+"/ingredients",
         dataType: "json", // data type of response
         success: function(data){
             if($('title').html().toLowerCase()=='menu') {
                 inflateAdminMenu(data);
             } else if($('title').html().toLowerCase()=='availability') {
-                inflateChecMenu(data);
+                inflateChefMenu(data);
             } else {
                 console.log("ERROR: Could not load menu data for this page.");
             }
         }
-    });
+   });
 }
 
+//Add an ingredient to the database
 function addIngredient(type, name) {
   $.ajax({
      type: 'POST',
@@ -40,77 +43,58 @@ function addIngredient(type, name) {
   });
 }
 
+//TODO: Luke doesn't think this has a purpose, but you should correct him.
+function returnItem(ingredient, jsonObject){
+   for(var key in data){
+      for(var k=0; k<data[key].length; k++){
+         if(data[i][k].name===ingredient){
+            return [key, k];
+         }
+      }
+   }
+}
+
 /**************************/
 /*   Inflater functions   */
 /**************************/
 function inflateAdminMenu(data) {
-
+    //TODO: write this.
 }
 
-function inflateChecMenu(data) { 
+function inflateChefMenu(data) {
+    for(var key in data){
+        $(".mainForm").append("<div></div>");
+        var currentItem=".mainForm div:last-child";
+        $(currentItem).append("<h2>"+key+"</h2>");
+        for(var i=0; i<data[key].length; i++){
+            var allPurpose=data[key][i].name;
+            $(currentItem).append("<label for=\""+allPurpose+"\">"+allPurpose+"</label>");
+            $(currentItem).append("<input id=\""+allPurpose+"\" type=\"checkbox\"><br/>");
+            if(data[key][i].available==1){
+                var evil=$(currentItem+" input:nth-last-child(2)");
+                $(evil).prop("checked", true);
+            }
+        }
+    }
+    $(".mainForm").append("<input id=\"availButton\" type=\"submit\">");
+    $("#availButton").click(function(){
+        var switchOn = new Array;
+        var switchOff = new Array;
+        var g=0;
+        for(var key in data){
+            g++;
+            for(var h=0; h<data[key].length; h++){
+                var inputPos="body > div > form > div:nth-child("+(g)+") > input:nth-child("+((h*3)+3)+")";
+                //console.log(data[key][h].name+",--- "+data[key][h].available+", ---"+$(inputPos).is(":checked"));
+                if(data[key][h].available!=$(inputPos).is(":checked")){
+                    if(data[key][h].available==1)
+                        switchOff.push(data[key][h].name);
+                    if(data[key][h].available==0)
+                        switchOn.push(data[key][h].name);
+                    //console.log(switchOn);
+                }
+            }
+        }
 
-    $('#menuForm').append("<ul id=\"basesMenu\">");  
-    for (var i=0; i<data['Bases'].length; i++){
-        $('#menuForm').append("<li> <input type=\"radio\" name=\"baseType\" id=\"" + data['Bases'][i] + "\" value=\"" + data['Bases'][i] + "\" required> <label for=\"" + data['Bases'][i] + "\">" + data['Bases'][i] + "</label></li>");
-    }
-    $('#menuForm').append("</ul><ul id=\"breadsMenu\">");
-    for (var i=0; i<data['Breads'].length; i++){
-        $('#menuForm').append("<li> <input type=\"radio\" name=\"breadType\" id=\"" + data['Breads'][i] + "\" value=\"" + data['Breads'][i] + "\" required> <label for=\"" + data['Breads'][i] + "\">" + data['Breads'][i] + "</label></li>");
-    }
-    $('#menuForm').append("</ul><ul id=\"cheeseMenu\">");
-    for (var i=0; i<data['Cheeses'].length; i++){
-        $('#menuForm').append("<li> <input type=\"radio\" name=\"cheeseType\" id=\"" + data['Cheeses'][i] + "\" value=\"" + data['Cheeses'][i] + "\" required> <label for=\"" + data['Cheeses'][i] + "\">" + data['Cheeses'][i] + "</label></li>");
-    }
-    $('#menuForm').append("</ul><ul id=\"toppingsMenu\">");
-    for (var i=0; i<data['Toppings'].length; i++){
-        $('#menuForm').append("<li> <input type=\"checkbox\" name=\"toppingType\" id=\"" + data['Toppings'][i] + "\" value=\"" + data['Toppings'][i] + "\"> <label for=\"" + data['Toppings'][i] + "\">" + data['Toppings'][i] + "</label></li>");
-    }
-    $('#menuForm').append("</ul><ul id=\"fryMenu\">");
-    for (var i=0; i<data['Fries'].length; i++){
-        $('#menuForm').append("<li> <input type=\"radio\" name=\"friesType\" id=\"" + data['Fries'][i] + "\" value=\"" + data['Fries'][i] + "\" required> <label for=\"" + data['Fries'][i] + "\">" + data['Fries'][i] + "</label></li>");
-    }
-
-    $('#menuForm').append("</ul><input type=\"submit\" value=\"Submit Order\">");
-
-    console.log("done inflating chef menu");
-
+    });
 }
-// function getMenuData() {
-//     $.ajax({
-//         type: 'GET',
-//         url: rootURL+"/menu",
-//         dataType: "json", // data type of response
-//         success: function(data){
-
-//             if(document.getElementsByClassName("mainForm").length>0){
-//             loadAvailChart(data);
-//             }  
-
-//             $('#menuForm').append("<ul id=\"basesMenu\">");  
-//             for (var i=0; i<data['Bases'].length; i++){
-//                 $('#menuForm').append("<li> <input type=\"radio\" name=\"baseType\" id=\"" + data['Bases'][i] + "\" value=\"" + data['Bases'][i] + "\" required> <label for=\"" + data['Bases'][i] + "\">" + data['Bases'][i] + "</label></li>");
-//             }
-//             $('#menuForm').append("</ul><ul id=\"breadsMenu\">");
-//             for (var i=0; i<data['Breads'].length; i++){
-//                 $('#menuForm').append("<li> <input type=\"radio\" name=\"breadType\" id=\"" + data['Breads'][i] + "\" value=\"" + data['Breads'][i] + "\" required> <label for=\"" + data['Breads'][i] + "\">" + data['Breads'][i] + "</label></li>");
-//             }
-//             $('#menuForm').append("</ul><ul id=\"cheeseMenu\">");
-//             for (var i=0; i<data['Cheeses'].length; i++){
-//                 $('#menuForm').append("<li> <input type=\"radio\" name=\"cheeseType\" id=\"" + data['Cheeses'][i] + "\" value=\"" + data['Cheeses'][i] + "\" required> <label for=\"" + data['Cheeses'][i] + "\">" + data['Cheeses'][i] + "</label></li>");
-//             }
-//             $('#menuForm').append("</ul><ul id=\"toppingsMenu\">");
-//             for (var i=0; i<data['Toppings'].length; i++){
-//                 $('#menuForm').append("<li> <input type=\"checkbox\" name=\"toppingType\" id=\"" + data['Toppings'][i] + "\" value=\"" + data['Toppings'][i] + "\"> <label for=\"" + data['Toppings'][i] + "\">" + data['Toppings'][i] + "</label></li>");
-//             }
-//             $('#menuForm').append("</ul><ul id=\"fryMenu\">");
-//             for (var i=0; i<data['Fries'].length; i++){
-//                 $('#menuForm').append("<li> <input type=\"radio\" name=\"friesType\" id=\"" + data['Fries'][i] + "\" value=\"" + data['Fries'][i] + "\" required> <label for=\"" + data['Fries'][i] + "\">" + data['Fries'][i] + "</label></li>");
-//             }
-
-//             $('#menuForm').append("</ul><input type=\"submit\" value=\"Submit Order\">");
-
-//         }
-
-//    });
-
-// }
