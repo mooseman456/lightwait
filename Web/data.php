c0cad4653d393e328cd267ed2bf189668661574e
LukeOglesbee <loglesbee@smu.edu>
2014-04-12 11:52:03 -0500

    data.php

---
+++ Web/data.php
@@ -0,0 +1,10 @@
+<?php
+	$navElements=array("back"=>"administrator.php","menu"=>"menu.php","logout"=>"logout.php");
+	$pageTitle="Data";
+	$javascript="";
+	include('include/header.php');
+?>
+
+</body>
+
+</html>
