986c158aea21b720d596af8d0e3b77e55f94e23c
LukeOglesbee <loglesbee@smu.edu>
2014-04-12 13:00:42 -0500

    users

--- Web/users.php
+++ Web/users.php
@@ -11,9 +11,24 @@
 ?>
 
 	<h1>Create Chef Account</h1>
+	<form id="createChefAccountForm" method="POST" action="#">
+		<input class="textForm" type="email" name="email" placeholder="Email" required>
+		<input class="textForm" type="password" name="password" pattern=".{8,20}" placeholder="Password" required>
+		<input type="text" name="fName" placeholder="First Name" required>
+		<input type="text" name="lName" placeholder="Last Name" required>
+		<input type="tel" name="phone" placeholder="Phone Number" required>
+		<input class="formSubmit" type="submit" value="Create Account">
+	</form>
 
 	<h1>Create Administrator Account</h1>
-	<form action="#"></form>
+	<form id="createAdminAccountForm" method="POST" action="#">
+		<input class="textForm" type="email" name="email" placeholder="Email" required>
+		<input class="textForm" type="password" name="password" pattern=".{8,20}" placeholder="Password" required>
+		<input type="text" name="fName" placeholder="First Name" required>
+		<input type="text" name="lName" placeholder="Last Name" required>
+		<input type="tel" name="phone" placeholder="Phone Number" required>
+		<input class="formSubmit" type="submit" value="Create Account">
+	</form>
 </body>
 
 </html>
