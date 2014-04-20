a6b18a17647b5d201a937e64a8167f6abad6b482
LukeOglesbee <loglesbee@smu.edu>
2014-04-19 21:19:55 -0500

    Data with search group method

--- Web/data.php
+++ Web/data.php
@@ -17,77 +17,43 @@
 	include('include/header.php');
 ?>
 
-<div id="queryInput">	
-	<form action="#" method="GET">
-		<!-- <fieldset>
-			<input type="radio" name="category" value="Base" id="baseCheckbox" />
-			<label for="baseCheckbox">Base</label>
-			<input type="radio" name="category" value="Bread" id="breadCheckbox"/>
-			<label for="breadCheckbox">Bread</label>
-			<input type="radio" name="category" value="Cheese" id="cheeseCheckbox" />
-			<label for="cheeseCheckbox">Cheese</label>
-			<input type="radio" name="category" value="Toppings" id="toppingsCheckbox"/>
-			<label for="toppingsCheckbox">Toppings</label>
-			<input type="radio" name="category" value="Fries" id="friesCheckbox"/>
-			<label for="friesCheckbox">Fries</label>
-		</fieldset> -->
-
-
+<div class="box data" id="queryInput">	
+	<form action="#" method="GET" name="searchGroup1">
 		<fieldset>
-			<legend>Date and Time</legend>
-			<input type="date" name="beginDate" id="startTime"/>
-			<input type="time" name="startTime" id="startTime"/>
-			<label for="beginDate">Before</label>
-			<input type="date" name="afterDate" id="afterDate"/>
-			<input type="time" name="afterTime" id="afterTime"/>
-			<label for="beginDate">After</label>
+			<label for="with">With</label>
+			<input type="text" placeholder="ingredients" name="with" id="with" />
+			<input type="radio" name="andor" value="and" id="andRadioWith" />
+			<label for="andRadioWith">And</label>
+			<input type="radio" name="andor" value="or" id="orRadioWith" />
+			<label for="orRadioWith">Or</label>
 		</fieldset>
-
 		<fieldset>
-			<legend>Count</legend>
-			<input type="checkbox" name="count" value="count" id="count">
-			<label for="count">Count</label>
+			<label for="without">Without</label>
+			<input type="text" placeholder="ingredients" name="without" id="without" />
+			<input type="radio" name="andor" value="and" id="andRadioWithout" />
+			<label for="andRadioWithout">And</label>
+			<input type="radio" name="andor" value="or" id="orRadioWithout" />
+			<label for="orRadioWithout">Or</label>
 		</fieldset>
-		
-		<fieldset name="Sorting">
-			<legend>Sort</legend>
-			<input type="radio" name="sort" value="sortAscend" id="sortAscend"/ >
-			<label for="sortAscend">Sort Ascending</label>
-			<input type="radio" name="sort" value="sortDescend" id="sortDescend" />
-			<label for="sortDescend">Sort Descending</label>
-			<input type="radio" name="sort" value="sortNone" id="sortNone" />
-			<label for="sortNone">No Sorting</label>
+		<fieldset>
+			<label for="dateGT">After</label>
+			<input type="date" name="dateGT" id="dateGT" />
+			<input type="time" name="timeGT" id="timeGT" />
 		</fieldset>
-
-
-
-		<fieldset id="hasIngredientsFormArea">
-			<legend>Contains</legend>
+		<fieldset>
+			<label for="dateLT">Before</label>
+			<input type="date" name="dateLT" id="dateLT" />
+			<input type="time" name="timeLT" id="timeLT" />
 		</fieldset>
-
-		<fieldset id="hasNotIngredientsFormArea">
-			<legend>Does not Contain</legend>
-			
+		<fieldset>
+			<label for="color">Color</label>
+			<input type="color" name="color" />
 		</fieldset>
-
-		<!-- <fieldset>
-			<input type="text" name="hasAllIngredients" placeholder="Has All" id="hasAllIngredient" />
-			<input type="button" value="Add">
-			<div class="ingredientList">Ingredients</div>
-			<input type="text" name="hasNotAllIngredients" placeholder="Has Not All" id="hasNotAllIngredients" />
-			<input type="button" value="Add">
-			<div class="ingredientList">Ingredients</div>
-			<input type="text" name="hasAnyIngredient" placeholder="Has Any" id="hasAnyIngredient" />
-			<input type="button" value="Add">
-			<div class="ingredientList">Ingredients</div>
-			<input type="text" name"hasNotAnyIngredient" placeholder="Has Not Any" id"hasNotAnyIngredient" />
-			<input type="button" value="Add">
-			<div class="ingredientList">Ingredients</div>
-		</fieldset> -->
-
-
-		<input type="submit" value="Retrieve" />
+		<input type="button" value="Add Search Group" />
+		<input type="button" value="Remove Search Group" />
 	</form>
+
+	<input type="button" value="Query" />
 </div>
 
 <div id="visuals">
