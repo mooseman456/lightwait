8ff88cb3161d8cedbfafefffcd6106c54e7c1722
LukeOglesbee <loglesbee@smu.edu>
2014-05-04 15:00:57 -0500

    queue interval

--- Web/javascript/queue.js
+++ Web/javascript/queue.js
@@ -10,7 +10,7 @@ var currentPage=1;
 
 $(document).ready(function() {
 	getActiveOrders();
-    setInterval(function(){getActiveOrders(); console.log("refresh");},5000);
+    setInterval(function(){getActiveOrders();},5000);
 
     /** Set height of queueWindow to fill remaining window afer navbar */
     setHeight();
